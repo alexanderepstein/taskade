68e31a5c6173462859160c01c879054661eecaf3
Alex Epstein <alexanderepstein@tuta.io>
2024-08-17 14:28:59 -0400

    Initial Commit

---
+++ src/tanto/__init__.py
@@ -0,0 +1,4 @@
+from tanto._base import Graph, Task, aexecute_graph, execute_graph
+from tanto._decorator import get_graph, task
+
+__all__ = ["task", "Graph", "Task", "aexecute_graph", "execute_graph", "get_graph"]
